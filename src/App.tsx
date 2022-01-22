0596e69ca7c4364837b52e3ae1ef67c549e415d8
Sarah Scardini <sarahscardini@gmail.com>
2022-01-22 03:12:44 -0300

    move files to upper directory

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { Routes } from "./routes";
+
+const App = () => {
+  return (
+    <>
+      <Toaster />
+      <Routes />
+    </>
+  );
+};
+
+export default App;
