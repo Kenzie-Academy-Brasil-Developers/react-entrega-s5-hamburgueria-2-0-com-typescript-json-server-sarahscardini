53abbd82f6ce49fc81f45e6f63475dc5954f577d
Sarah Scardini <sarahscardini@gmail.com>
2022-01-28 07:53:38 -0300

    refactor(notFound): indent notFound page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,41 +1,41 @@
 import { Flex, Box, Heading, Button, Image } from "@chakra-ui/react";
+import { theme } from "../../styles/theme";
 import { useHistory } from "react-router";
 import ImgNotFound from "../../assets/error.gif";
-import { theme } from "../../styles/theme";
 
 export const NotFound = () => {
   const history = useHistory();
 
   return (
     <Flex
-      padding={["15px", "15px", "0px", "0px"]}
       alignItems="center"
-      justifyContent="space-evenly"
-      height="100vh"
       flexDirection={["column", "column", "row", "row"]}
+      height="100vh"
+      justifyContent="space-evenly"
+      padding={["15px", "15px", "0px", "0px"]}
     >
       <Box mt="4">
         <Heading textAlign="center" fontSize="1.5rem">
           Página não encontrada
         </Heading>
         <Button
-          mt="30px"
           bg={theme.colors.primaryPalette.primary}
-          h="60px"
           color="white"
+          h="60px"
+          mt="30px"
+          onClick={() => history.push("/")}
           w="100%"
           _hover={{ bg: theme.colors.primaryPalette["primary.50"] }}
-          onClick={() => history.push("/")}
         >
           Voltar para a home
         </Button>
       </Box>
       <Image
-        src={ImgNotFound}
         alt="Sorry, page not found"
-        w="50vw"
         h="auto"
         maxW="600px"
+        src={ImgNotFound}
+        w="50vw"
       />
     </Flex>
   );
